c70cbb33cf18b1426179cf5845e83c398748b3d7
sytect <sytect@hotmail.com>
2016-09-09 12:00:53 -0400

    clean slate ready for tank drive

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/TeleOp.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by khenr on 1/13/2017.
+ */
+
+public class TeleOp extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        while (opModeIsActive()) {
+
+        }
+    }
+
+
+}
